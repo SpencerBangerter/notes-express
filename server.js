fccb3aa105636d806ba59b0ee907573bd9234aa8
Spencer Bangterer <bangerter.spencer@gmail.com>
2020-02-29 10:07:09 -0700

    Post now re-writing the json file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+//Constants
 const express = require('express');
 const fs = require("fs");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
 
 
 
@@ -21,6 +26,28 @@ app.get("/", function(req, res) {
 app.get('/notes', function(req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+//Save note route
+
+app.post('/api/notes', function (req, res) {
+  let newNote = req.body
+
+  fs.readFile("db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+
+    let notesArray = JSON.parse(data);
+
+    notesArray.push(newNote)
+    console.log(notesArray)
+
+    fs.writeFile("db/db.json", JSON.stringify(notesArray), 'utf8', (err) =>{
+      if (err) throw err;
+      console.log('New Notes have been saved')
+    })
+  })
+});
+
+
 
 // Starts the server to begin listening
 // =============================================================
